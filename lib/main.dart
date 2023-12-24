824f7eaeae43a1fef70528ceaf0df35d312ed27f
MoldyHacker <56985918+MoldyHacker@users.noreply.github.com>
2023-12-24 14:00:54 -0600

    Add Expenses widget to MaterialApp, added an expense model, added the uuid package

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'package:expense_tracker/expenses.dart';
+
 void main() {
-  runApp(MaterialApp(
-    home: // TODO: Add the Home widget
-  ));
+  runApp(
+    const MaterialApp(
+    home: Expenses(),
+    ),
+  );
 }
